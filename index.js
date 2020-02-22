3208727d93bb1c59ef9ee9a2c4b35fa46122cbd0
Niaina Michaël RAZAFIMANDIMBY (balzac le Geek) <michaniainar@gmail.com>
2020-02-23 00:34:24 +0300

    Verify options & form node

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 (function () {
-    const greatMessage = 'LightValidator has been instancied';
+    const greatMessage  = 'LightValidator has been instancied';
+    const errorStyle    = "color: #ffffff; background: #fd4042; padding: 5px; font-size: 15px;"
+
+    const ERR_MSG_FORM_ID_MISSED = '"id: YOUR_FORM_ID" option must be defined'
 
     // Ligth Form Validator constructor
     this.LightValidator = function () {
@@ -13,7 +16,36 @@
         // Create options by extending defaultOptions with the passed in arugments, assing defaultOptions otherwise
         this.options    = (arguments[0] && typeof arguments[0] === 'object') ? extendDefaultOptions(defaultOptions, arguments[0]) : defaultOptions
 
+        if (verifyOptions.call(this)) {
+            this.form = getForm(this.options.id)
+        }
+
         helloMsg()
+    }
+
+    function verifyOptions() {
+        const formId = this.options.id
+
+        if (!formId) {
+            throwError(ERR_MSG_FORM_ID_MISSED)
+        }
+
+        return true
+    }
+
+    const getForm = formId => {
+        const formNode = document.getElementById(formId)
+
+        if (!formNode) {
+            throwError(`Form with id "${formId}" not found`)
+        }
+
+        return formNode
+    }
+
+    const throwError = message => {
+        console.info(`%c ❌ ${ message } `, errorStyle)
+        throw message
     }
 
     // Extends defaultOptions with the provided options
