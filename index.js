fda1dab753efa8885356ddb20cdb150456f203e0
Niaina Michaël RAZAFIMANDIMBY (balzac le Geek) <michaniainar@gmail.com>
2020-02-22 23:22:39 +0300

    Add defaultOptions && custom options

--- index.js
+++ index.js
@@ -3,7 +3,30 @@
 
     // Ligth Form Validator constructor
     this.LightValidator = function () {
+        const defaultOptions = {
+            id: null,
+            className: 'light-validator'
+            // Future options goes here
+        }
+
+        // Plugin options
+        // Create options by extending defaultOptions with the passed in arugments, assing defaultOptions otherwise
+        this.options    = (arguments[0] && typeof arguments[0] === 'object') ? extendDefaultOptions(defaultOptions, arguments[0]) : defaultOptions
+
         helloMsg()
+    }
+
+    // Extends defaultOptions with the provided options
+    const extendDefaultOptions = (defaultOptions, options) => {
+        let property;
+
+        for (property in options) {
+            if (options.hasOwnProperty(property)) {
+                defaultOptions[property] = options[property]
+            }
+        }
+
+        return defaultOptions;
     }
 
     const helloMsg = () => {
