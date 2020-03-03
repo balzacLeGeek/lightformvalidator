f5c2ebeef63fe059abd79be52079989da1fc6214
Rado <radonirina.maminiaina@gmail.com>
2020-03-03 10:35:50 +0300

    Use promise instead of callback

--- README.md
+++ README.md
@@ -6,21 +6,24 @@
 
 [Light Form Validator][1] is a light **JS plugin** written with Vanilla JS, no dependencies, used to make a simple but strong form validation.
 
-Wanna try it?
-------------
+## Wanna try it?
+
 Great!!! Let's install this plugin
 
 ### For basic web project
+
 - Include this script in your html code
+
 ```html
 <script src="https://balzacLeGeek.github.io/lightformvalidator/index.js"></script>
 ```
 
 ### Using NPM / Yarn
+
 Installation steps goes here :)
 
-Documentation
-------------
+## Documentation
+
 ### API
 
 - [LightValidator instance](#new-lightvalidatoroptions)
@@ -28,9 +31,11 @@ Documentation
 - [Console is your friend](#console-is-your-friend)
 
 ### new LightValidator(options)
+
 Creates an instance of LightValidator.
 
 - `options` An options object
+
 ```javascript
 {
 	id: YOUR_FORM_ID, // Required option
@@ -38,69 +43,109 @@ Creates an instance of LightValidator.
 ```
 
 ### validate(callback)
+
 Process the form validation and pass the `Serialized form datas` to a callback function. If no callback function has provided, `Serialized form datas` are returned.
 
 ### Console is your friend
-- ***❌ Form with id "YOUR_FORM_ID" not found***
-You have provided a wrong id
-<p align="left">
-	<img src="assets/imgs/ERROR_FORM_NODE_NOT_FOUND.jpg">
-</p>
 
-- ***❌ Invalid Node Element***
-The given ID doesn't match to any **form** element.
-<p align="left">
-	<img src="assets/imgs/ERROR_NODE_INVALID.jpg">
-</p>
+- **_❌ Form with id "YOUR_FORM_ID" not found_**
+  You have provided a wrong id
 
-- ***❌ Form has error***
-One or more required fields are not valid
-<p align="left">
-	<img src="assets/imgs/ERROR_FORM_HAS_ERROR.jpg">
-</p>
+  <p align="left">
+  	<img src="assets/imgs/ERROR_FORM_NODE_NOT_FOUND.jpg">
+  </p>
+
+- **_❌ Invalid Node Element_**
+  The given ID doesn't match to any **form** element.
+
+  <p align="left">
+  	<img src="assets/imgs/ERROR_NODE_INVALID.jpg">
+  </p>
+
+- **_❌ Form has error_**
+  One or more required fields are not valid
+
+  <p align="left">
+  	<img src="assets/imgs/ERROR_FORM_HAS_ERROR.jpg">
+  </p>
 
 - **🥳 Everything is OK**
-<p align="left">
-	<img src="assets/imgs/EVERYTHING_IS_OK.jpg">
-</p>
+  <p align="left">
+  	<img src="assets/imgs/EVERYTHING_IS_OK.jpg">
+  </p>
 
 ## Usage example
+
 The following HTML code uses boostrap form.
 
 ```html
 <form id="myForm" novalidate>
-	<div class="form-group">
-		<label for="name">Fullname</label>
-		<input type="text" name="name" id="name" class="form-control" placeholder="Enter your fullname" required>
-	</div>
-	<div class="form-group">
-		<label for="email">Email address</label>
-		<input type="email" name="email" class="form-control" id="email" placeholder="Enter email" required>
-	</div>
-	<div class="form-group">
-		<label for="password">Password</label>
-		<input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
-	</div>
-	<div class="form-group">
-		<label for="sexe">Gender</label>
-		<div class="custom-control custom-radio custom-control-inline">
-			<input class="custom-control-input" type="radio" id="gender-mr" name="gender" value="mr" checked>
-			<label class="custom-control-label" for="gender-mr">Mr</label>
-		</div>
-		<div class="custom-control custom-radio custom-control-inline">
-			<input class="custom-control-input" type="radio" id="gender-mme" name="gender" value="mme">
-			<label class="custom-control-label" for="gender-mme">Mme</label>
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="langage">langage</label>
-		<select class="form-control" name="langage" id="langage">
-			<option value="MG">Malagasy</option>
-			<option value="FR">French</option>
-			<option value="EN">English</option>
-		</select>
-	</div>
-	<button type="submit" class="btn btn-primary">Submit</button>
+  <div class="form-group">
+    <label for="name">Fullname</label>
+    <input
+      type="text"
+      name="name"
+      id="name"
+      class="form-control"
+      placeholder="Enter your fullname"
+      required
+    />
+  </div>
+  <div class="form-group">
+    <label for="email">Email address</label>
+    <input
+      type="email"
+      name="email"
+      class="form-control"
+      id="email"
+      placeholder="Enter email"
+      required
+    />
+  </div>
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input
+      type="password"
+      name="password"
+      class="form-control"
+      id="password"
+      placeholder="Password"
+      required
+    />
+  </div>
+  <div class="form-group">
+    <label for="sexe">Gender</label>
+    <div class="custom-control custom-radio custom-control-inline">
+      <input
+        class="custom-control-input"
+        type="radio"
+        id="gender-mr"
+        name="gender"
+        value="mr"
+        checked
+      />
+      <label class="custom-control-label" for="gender-mr">Mr</label>
+    </div>
+    <div class="custom-control custom-radio custom-control-inline">
+      <input
+        class="custom-control-input"
+        type="radio"
+        id="gender-mme"
+        name="gender"
+        value="mme"
+      />
+      <label class="custom-control-label" for="gender-mme">Mme</label>
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="langage">langage</label>
+    <select class="form-control" name="langage" id="langage">
+      <option value="MG">Malagasy</option>
+      <option value="FR">French</option>
+      <option value="EN">English</option>
+    </select>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 ```
 
@@ -108,30 +153,29 @@ The following HTML code uses boostrap form.
 // Create an instance of the LightValidator
 const myForm = new LightValidator({
 	id: 'myForm', // Oour form id's
-}) 
-
-// Use validate() method to process validation
-myForm.validate(datas  => {
-	// Your callback function
-	// Ex: Send `datas` object within a POST request
 })
 
-// If case you just need the validated datas
-// It returns the validated and serialized form datas
-const myFormDatas = myForm.validate()
+// Use validate() method to process validation
+myForm.validate().then((datas)  => {
+	// Your callback function
+	// Ex: Send `datas` object within a POST request
+}))
 ```
 
 ## TODO
-- [ ] Custom error message from a `data-error` attribute
+
+- [+] Custom error message from a `data-error` attribute
 - [ ] Password validation options. `false` by default
 - [ ] Password confirmation validation
 
 ## Credits
+
 [Light Form Validator][1] was inspired by an old code of my friend [Rado](https://github.com/radonirinamaminiaina).
 
 Light Form Validator logo fond was picked from [Champagne & Limousines](https://www.dafont.com/fr/champagne-limousines.font) (Dafont).
 
 ## License
+
 MIT
 
 [1]: https://balzaclegeek.github.io/lightformvalidator
--- index.js
+++ index.js
@@ -1,243 +1,260 @@
-(function () {
-    const greatMessage  = 'LightValidator has been instancied';
-    const errorStyle    = "color: #ffffff; background: #fd4042; padding: 5px; font-size: 15px;"
+(function() {
+  const greatMessage = "LightValidator has been instancied";
+  const errorStyle =
+    "color: #ffffff; background: #fd4042; padding: 5px; font-size: 15px;";
 
-    const ERR_MSG_FORM_ID_MISSED = '"id: YOUR_FORM_ID" option must be defined'
+  const ERR_MSG_FORM_ID_MISSED = '"id: YOUR_FORM_ID" option must be defined';
 
-    // Ligth Form Validator constructor
-    this.LightValidator = function () {
-        const defaultOptions = {
-            id: null,
-            className: 'light-validator'
-            // Future options goes here
+  // Ligth Form Validator constructor
+  this.LightValidator = function() {
+    const defaultOptions = {
+      id: null,
+      className: "light-validator"
+      // Future options goes here
+    };
+
+    // Plugin options
+    // Create options by extending defaultOptions with the passed in arugments, assing defaultOptions otherwise
+    this.options =
+      arguments[0] && typeof arguments[0] === "object"
+        ? extendDefaultOptions(defaultOptions, arguments[0])
+        : defaultOptions;
+
+    if (verifyOptions.call(this)) {
+      this.form = getForm(this.options.id);
+    }
+
+    helloMsg();
+  };
+
+  LightValidator.prototype.validate = function() {
+    return new Promise(resolve => {
+      const _ = this;
+      const form = _.form;
+
+      onKeyUp(form);
+
+      form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const formDatas = serializeForm(form);
+
+        if (formHasError(form)) {
+          throwError(`Form has error`);
         }
 
-        // Plugin options
-        // Create options by extending defaultOptions with the passed in arugments, assing defaultOptions otherwise
-        this.options    = (arguments[0] && typeof arguments[0] === 'object') ? extendDefaultOptions(defaultOptions, arguments[0]) : defaultOptions
+        resolve(formDatas);
 
-        if (verifyOptions.call(this)) {
-            this.form = getForm(this.options.id)
+        return formDatas;
+      });
+    });
+  };
+
+  /**
+   *
+   *
+   * @param {Node} form
+   * @return {Boolean}
+   */
+  const formHasError = form => {
+    var errorList = [];
+
+    loopElements(form, element => {
+      errorList.push(processFielValidation(element, false));
+    });
+
+    return errorList.indexOf(true) !== -1;
+  };
+
+  /**
+   * Loops all form elements and process item in the given callback function
+   *
+   * @param {Node} form
+   * @param {Function} callback A callback function
+   */
+  const loopElements = (form, callback) => {
+    getElements(form).forEach(element => {
+      callback(element);
+    });
+  };
+
+  const getFormChildren = (form, type, selector) => {
+    return form[`querySelector${type}`](selector);
+  };
+
+  const getElements = form => {
+    return getFormChildren(form, "All", "[name]");
+  };
+
+  const getSubmitButton = form => {
+    return getFormChildren(form, "", '[type="submit"]');
+  };
+
+  const processFielValidation = (element, checkError) => {
+    const $parent = element.closest("div");
+
+    if (element.required) {
+      if (element.value === "") {
+        element.classList.add("invalid-field");
+        $parent.classList.add("invalid-field-wrapper");
+
+        showInvalidMessage($parent, element.name);
+
+        if (checkError !== undefined) {
+          checkError = true;
         }
+      } else {
+        $parent.classList.remove("invalid-field-wrapper");
+        element.classList.remove("invalid-field");
 
-        helloMsg()
-    }
+        if (element.type === "email") {
+          const emailValue = element.value;
 
-    LightValidator.prototype.validate = function(callback) {
-        const _ = this;
-        const form = _.form
-
-        onKeyUp(form);
-
-        form.addEventListener('submit', function (e) {
-            e.preventDefault()
-
-            if (formHasError(form)) {
-                throwError(`Form has error`)
-            }
-
-            const formDatas = serializeForm(form)
-
-            if (callback && typeof(callback) === "function") {
-                callback(formDatas)
-            }
-
-            return formDatas;
-        })
-    }
-
-    /**
-     * 
-     * 
-     * @param {Node} form 
-     * @return {Boolean}
-     */
-    const formHasError = form => {
-        var errorList = [];
-
-        loopElements(form, element => {
-            errorList.push(processFielValidation(element, false));
-        })
-
-        return errorList.indexOf(true) !== -1;
-    }
-
-    /**
-     * Loops all form elements and process item in the given callback function
-     * 
-     * @param {Node} form 
-     * @param {Function} callback A callback function
-     */
-    const loopElements = (form, callback) => {
-        getElements(form).forEach(element => {
-            callback(element)
-        });
-    }
-
-    const getFormChildren = (form, type, selector) => {
-        return form[`querySelector${type}`](selector);
-    }
-
-    const getElements = form => {
-        return getFormChildren(form, 'All', '[name]');
-    }
-
-    const getSubmitButton = form => {
-        return getFormChildren(form, '', '[type="submit"]');
-    }
-
-    const processFielValidation = (element, checkError) => {
-        const $parent = element.closest('div');
-
-        if (element.required) {
-            if (element.value === '') {
-                element.classList.add('invalid-field');
-                $parent.classList.add('invalid-field-wrapper');
-
-                showInvalidMessage($parent, element.name);
-
-                if (checkError !== undefined) {
-                    checkError = true;
-                }
-            } else {
-                $parent.classList.remove('invalid-field-wrapper');
-                element.classList.remove('invalid-field');
-
-                if (element.type === 'email') {
-                    const emailValue = element.value
-
-                    if (!validEmail(emailValue)) {
-                        showInvalidMessage($parent, '', `<strong><em>${ emailValue }</em></strong> is not an email address valid`);
-                    } else {
-                        removeInvalidMessage($parent);
-                    }
-                } else {
-                    removeInvalidMessage($parent);
-                }
-            }
+          if (!validEmail(emailValue)) {
+            showInvalidMessage(
+              $parent,
+              "",
+              `<strong><em>${emailValue}</em></strong> is not an email address valid`
+            );
+          } else {
+            removeInvalidMessage($parent);
+          }
+        } else {
+          removeInvalidMessage($parent);
         }
-
-        return checkError;
+      }
     }
 
-    /**
-     * Trigger form validation on form element KeyUp
-     * 
-     * @param {Node} form The form node
-     */
-    function onKeyUp(form) {
-        loopElements(form, function(element) {
-            element.onkeyup = function () {
-                processFielValidation(this);
-            }
-        })
+    return checkError;
+  };
+
+  /**
+   * Trigger form validation on form element KeyUp
+   *
+   * @param {Node} form The form node
+   */
+  function onKeyUp(form) {
+    loopElements(form, function(element) {
+      element.onkeyup = function() {
+        processFielValidation(this);
+      };
+    });
+  }
+
+  /**
+   * Serializes form
+   *
+   * @param {Node} form The form node
+   * @return {Object} The form datas { name: value }
+   */
+  const serializeForm = form => {
+    const serialized = {};
+
+    loopElements(form, element => {
+      const elementType = element.type;
+
+      if (
+        (elementType === "radio" && element.checked) ||
+        elementType !== "radio"
+      ) {
+        serialized[element.name] = element.value;
+      }
+    });
+
+    return serialized;
+  };
+
+  /**
+   * Resets form elements
+   *
+   * @param {Node} form The form node
+   */
+  const reset = form => {
+    loopElements(form, element => {
+      switch (element.tagName.toLowerCase()) {
+        case "select":
+          element.querySelector("option").selected = "selected";
+          break;
+        default:
+          element.value = "";
+      }
+    });
+  };
+
+  function removeInvalidMessage(parent) {
+    const $invalidMessage = getFormChildren(parent, "", ".invalid-message");
+
+    if ($invalidMessage) {
+      $invalidMessage.parentNode.removeChild($invalidMessage);
+    }
+  }
+
+  function showInvalidMessage(parent, fieldName, custom) {
+    const div = document.createElement("div");
+
+    removeInvalidMessage(parent);
+
+    div.classList.add("invalid-message");
+
+    div.innerHTML = custom
+      ? custom
+      : `<strong><em>${fieldName}</em></strong> is required.`;
+
+    parent.appendChild(div);
+  }
+
+  const validEmail = value => /^.+@.+\..{2,}$/.test(value);
+
+  function verifyOptions() {
+    const formId = this.options.id;
+
+    if (!formId) {
+      throwError(ERR_MSG_FORM_ID_MISSED);
     }
 
-    /**
-     * Serializes form
-     * 
-     * @param {Node} form The form node
-     * @return {Object} The form datas { name: value }
-     */
-    const serializeForm = form => {
-        const serialized = {};
+    return true;
+  }
 
-        loopElements(form, element => {
-            const elementType = element.type
+  const getForm = formId => {
+    const formNode = document.getElementById(formId);
 
-            if ((elementType === 'radio' && element.checked) || elementType !== 'radio') {
-                serialized[element.name] = element.value;
-            }
-        })
-
-        return serialized;
+    if (!formNode) {
+      throwError(`Form with id "${formId}" not found`);
     }
 
-    /**
-     * Resets form elements
-     * 
-     * @param {Node} form The form node
-     */
-    const reset = form => {
-        loopElements(form, element => {
-            switch (element.tagName.toLowerCase()) {
-                case 'select':
-                    element.querySelector('option').selected = 'selected';
-                    break;
-                default:
-                    element.value = '';
-            }
-        })
+    const nodeName = formNode.nodeName.toLowerCase();
+
+    if (nodeName !== "form") {
+      throwError(
+        `LightValidator element must be a <form>, <${nodeName}> given`
+      );
     }
 
-    function removeInvalidMessage(parent) {
-        const $invalidMessage = getFormChildren(parent, '', '.invalid-message');
+    return formNode;
+  };
 
-        if ($invalidMessage) {
-            $invalidMessage.parentNode.removeChild($invalidMessage);
-        }
-    }
-    
-    function showInvalidMessage(parent, fieldName, custom) {
-        removeInvalidMessage(parent);
+  const throwError = message => {
+    console.info(`%c ❌ ${message} `, errorStyle);
+    throw message;
+  };
 
-        const div = document.createElement('div');
+  // Extends defaultOptions with the provided options
+  const extendDefaultOptions = (defaultOptions, options) => {
+    let property;
 
-        div.classList.add('invalid-message');
-
-        div.innerHTML = custom ? custom : `<strong><em>${fieldName}</em></strong> is required.`;
-
-        parent.appendChild(div);
+    for (property in options) {
+      if (options.hasOwnProperty(property)) {
+        defaultOptions[property] = options[property];
+      }
     }
 
-    const validEmail = value => /^.+@.+\..{2,}$/.test(value)
+    return defaultOptions;
+  };
 
-    function verifyOptions() {
-        const formId = this.options.id
-
-        if (!formId) {
-            throwError(ERR_MSG_FORM_ID_MISSED)
-        }
-
-        return true
-    }
-
-    const getForm = formId => {
-        const formNode = document.getElementById(formId)
-
-        if (!formNode) {
-            throwError(`Form with id "${formId}" not found`)
-        }
-
-        const nodeName = formNode.nodeName.toLowerCase()
-
-        if (nodeName !== 'form') {
-            throwError(`LightValidator element must be a <form>, <${ nodeName }> given`)
-        }
-
-        return formNode
-    }
-
-    const throwError = message => {
-        console.info(`%c ❌ ${ message } `, errorStyle)
-        throw message
-    }
-
-    // Extends defaultOptions with the provided options
-    const extendDefaultOptions = (defaultOptions, options) => {
-        let property;
-
-        for (property in options) {
-            if (options.hasOwnProperty(property)) {
-                defaultOptions[property] = options[property]
-            }
-        }
-
-        return defaultOptions;
-    }
-
-    const helloMsg = () => {
-        // console.log(`%c Hello`, 'font-weight: bold, font-size: 18px, color: #1d1e3d')
-        console.log(`%c 🥳 ${ greatMessage } `, "color: #1d1e3d; background: #bcbcbc; padding: 5px; font-size: 15px;");
-    }
+  const helloMsg = () => {
+    // console.log(`%c Hello`, 'font-weight: bold, font-size: 18px, color: #1d1e3d')
+    console.log(
+      `%c 🥳 ${greatMessage} `,
+      "color: #1d1e3d; background: #bcbcbc; padding: 5px; font-size: 15px;"
+    );
+  };
 })();
